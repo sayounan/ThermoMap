96410e2ec3f18f2ee906735e28e70cab2046a738
syounan <sari.younan@gmail.com>
2024-04-29 20:38:21 -0600

    Added heat transfer algorithm

---
+++ Programs/Heat Transfer/Heat.h
@@ -0,0 +1,19 @@
+/*
+Created by Sari I. Younan
+29/04/2024 19:05:20
+Heat.h
+*/
+
+#ifndef UNTITLED_HEAT_H
+#define UNTITLED_HEAT_H
+
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <thread>
+#include "eigen-3.4.0/Eigen/Dense"
+
+using namespace std;
+
+
+#endif //UNTITLED_HEAT_H
